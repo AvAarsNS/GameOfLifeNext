dc1f3b771db25e1c544b757c4919018f6a4f98d8
Johan4839 <125651187+Johan4839@users.noreply.github.com>
2023-11-10 13:50:01 +0100

    chore: made first test case green

--- NOTES.md
+++ NOTES.md
@@ -4,5 +4,5 @@
 
 # API verbinding met backend
 - Gemockte call werkend
-  🙌- Start a new game with a glider, successful response, glider universe is found
+  ✅- Start a new game with a glider, successful response, glider universe is found
   - Start a new game with a glider, error receveid, no universe found
--- test/unit/backendapi.test.ts
+++ test/unit/backendapi.test.ts
@@ -5,24 +5,19 @@ import { gameOfLifeAPIJSON} from '../doubles/grid.double';
 describe('We will test the backend api here', () => {
     it('When we ask for a new game with glider pattern, and a succesful response is returned ', async () => {
         
-        axios.post.mockResolvedValue({});
+        axios.post.mockResolvedValue({ data: gameOfLifeAPIJSON });
 
         const responseBody = await postStartNewGame();
 
-        const requestBody = {
-            "width" : 20,
-            "height" : 20,
-            "pattern" : "glider"
-          }
+        const requestUrl = "http://localhost:3000/newgame";
+        const requestPayload = {
+          width: 20,
+          height: 20,
+          pattern: "glider",
+        };
 
-        const request = new Request('http://localhost:3000/newgame', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(requestBody)
-        });
-
-        expect(axios.post).toHaveBeenCalledWith(request);
+        expect(axios.post).toHaveBeenCalledWith(requestUrl, requestPayload);
         expect(axios.post).toHaveBeenCalledTimes(1);
-        expect(responseBody).toEqual({gameOfLifeAPIJSON});
+        expect(responseBody).toEqual(gameOfLifeAPIJSON);
     });
 });
