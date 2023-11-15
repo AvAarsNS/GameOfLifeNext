5d65f12834b5f7f6f312b550bfb5787bb0c323a7
mcoen93ns <mathijs.coenen@ns.nl>
2023-11-15 12:01:16 +0100

    chore: added some annotations

--- NOTES.md
+++ NOTES.md
@@ -2,7 +2,20 @@
 - Patroonlijstje namaken ✅
 - Start button d'rin joekelen 
 
-# API verbinding met backend
-- Gemockte call werkend
+# API connection with backend
+- Mock call to backend
   ✅- Start a new game with a glider, successful response, glider universe is found
-  - Start a new game with a glider, error receveid, no universe found
+  - Start a new game with a glider, error receveid, no universe returned
+
+# Maybe do these tests in Cypress? Spike?
+https://nextjs.org/docs/pages/building-your-application/optimizing/testing
+I mean this test: - Test that the DOM contains the correct universe
+
+# Test the gamegrid component which is actually a unit test that tests a nextJS component ......
+- Test that the DOM contains the correct universe
+- Create functionality that transforms the JSON universe to the grid
+
+# Investigate SuperTest
+https://www.npmjs.com/package/supertest
+
+# Learn to navigate by keyboard to prevent serious injury to our wrists and fingers
--- test/unit/backendapi.test.ts
+++ test/unit/backendapi.test.ts
@@ -2,14 +2,15 @@ import { mockedAxios as axios } from '../mocks/axios';
 import { postStartNewGame } from '../../src/backendapi';
 import { gameOfLifeAPIJSON} from '../doubles/grid.double';
 
-describe('We will test the backend api here', () => {
-    it('When we ask for a new game with glider pattern, and a succesful response is returned ', async () => {
+describe('This is the test suite for functionality that will enable the interaction with the Game of Life back-end', () => {
+    it('When we ask for a new game with glider pattern, this will be returned', async () => {
         
         axios.post.mockResolvedValue({ data: gameOfLifeAPIJSON });
 
         const responseBody = await postStartNewGame();
-
+        // TODO: use a double for this
         const requestUrl = "http://localhost:3000/newgame";
+        // TODO: use a double for this
         const requestPayload = {
           width: 20,
           height: 20,
@@ -18,6 +19,7 @@ describe('We will test the backend api here', () => {
 
         expect(axios.post).toHaveBeenCalledWith(requestUrl, requestPayload);
         expect(axios.post).toHaveBeenCalledTimes(1);
+        // TODO: update naming to make it clear that it is a stub. Do not remove it. It is Hybrid!
         expect(responseBody).toEqual(gameOfLifeAPIJSON);
     });
 });
