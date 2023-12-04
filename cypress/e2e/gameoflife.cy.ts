83207080a9bfb8d9510e4ef7227299ae18a1f117
mcoen93ns <mathijs.coenen@ns.nl>
2023-12-04 11:24:56 +0100

    build: finished first version of e2e

--- cypress/e2e/gameoflife.cy.ts
+++ cypress/e2e/gameoflife.cy.ts
@@ -1,18 +1,18 @@
+import { gameOfLifeAPIJSON } from '../../test/doubles/grid.double';
+
 describe('Time to play the game of life', () => {
   it('is able to play the first tick of a new game with a glider', () => {
     cy.visit('http://localhost:3000')
-    cy.get('[data-cy-radiobutton-pattern=Glider]').click()
+    cy.get('[data-cy-radiobutton-pattern=Glider]').click();
 
-    // Intercept the network request when the start button is clicked
+    // Intercept the network request when the start button is clicked. Some kind of mocking :)
     cy.intercept('POST', '/newgame*', {
       statusCode: 200,
-    //TODO: Add the response body of glider here
-    })
-    
-    cy.get('[data-cy-start-button]').click()
+      body: gameOfLifeAPIJSON
+    });
 
-    // Continue with the rest of your test
-    cy.wait(1000)
-    cy.get(`[data-cy-gridcoordinate="(0,0)"]`).should('have.class', 'alive')
+    cy.get('[data-cy-start-button]').click();
+
+    cy.get(`[data-cy-gridcoordinate="(0,0)"]`).should('have.class', 'alive');
   });
 });
